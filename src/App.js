6500994b74c483939975055748383558d7250912
diikshaaaa <diksha.grover@crownstack.com>
2022-01-29 18:21:54 +0530

    made react menu

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+// JavaScript Set is a collection of unique values. Each value can only occur once in a Set. A Set can hold any value of any data type.
+// new Set() - Creates a new Set
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
